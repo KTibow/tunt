287b581bb4ebbd375a4bb5b357fbac21e5fc4dcb
Kendell R <KTibow@users.noreply.github.com>
2021-03-17 15:37:38 -0700

    make flake happy

--- local_actions.py
+++ local_actions.py
@@ -1,12 +1,15 @@
+"""Module to quickly access common commands used during development."""
+
 import os
 
 option = input(
-    "What do you want to do? Run flake8 (1), run formatting (2), or run a local server (3)? "
+    "What do you want to do? "
+    + "Run flake8 (1), run formatting (2), or run a local server (3)? "
 )
 option = int(option)
 if option == 1:
     os.system("python -m flake8 .")
 elif option == 2:
-    os.system("python -m black . && npx prettier **\\*.css --write")
+    os.system(r"python -m black . && npx prettier **\*.css --write")
 elif option == 3:
     os.system("python -m flask run")
