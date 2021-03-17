bc592e217e425935c8938a18374dd9e1a2b4c250
Kendell R <KTibow@users.noreply.github.com>
2021-03-16 19:09:01 -0700

    Even more test fixes

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import requests
 
 
 def test_homepage_content():
-    home = requests.get("http://127.0.0.1:5000")
+    home = requests.get("http://127.0.0.1:5000/")
     original_page = open("assets/home.html")
     assert home.text == original_page.read()
     original_page.close()
