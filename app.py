11d911e9c460fe04d86b95367d3d5986e5fca693
Kendell R <KTibow@users.noreply.github.com>
2021-03-17 07:01:53 -0700

    Tests work... now fix everything else

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 """Server program for Tunt."""
 
 from flask import Flask
-import os, logging
+import os
+import logging
 import template
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ def render_home_page():
     """
     with open("assets/home.yaml") as home_page:
         return template.compile_template(home_page)
+
 
 if os.getenv("GITHUB_ACTIONS") is not None:
     flask_logger = logging.getLogger("werkzeug")
