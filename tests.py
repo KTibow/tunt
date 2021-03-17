d9ac62d19ab71bbd2ea7667706e77efd2fe33d13
Kendell R <KTibow@users.noreply.github.com>
2021-03-16 19:00:15 -0700

    Big tests commit

---
+++ tests.py
@@ -0,0 +1,7 @@
+import requests
+
+def test_homepage_content():
+    home = requests.get("http://127.0.0.1:5000")
+    original_page = open("assets/home.html")
+    assert home.text == original_page.read()
+    original_page.close()
