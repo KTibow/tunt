557e065416168cd1ab54de53291ea6a7d1f69fb0
Kendell R <KTibow@users.noreply.github.com>
2021-03-20 08:51:18 -0700

    Asset and template changes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, Response
 import os
 import logging
 import template
+import mimetypes
 
 app = Flask(__name__)
 
@@ -19,18 +20,19 @@ def render_home_page():
         return template.compile_template(home_page)
 
 
-@app.route("/css/<css_file_name>")
-def send_css_file(css_file_name):
-    """Return a CSS file.
+@app.route("/asset/<asset_file_name>")
+def send_asset(asset_file_name):
+    """Return an asset.
 
     Args:
-        css_file_name: The name of the CSS file.
+        asset_file_name: The path of the asset file relative to the assets folder.
 
     Returns:
-        The CSS file specified in the URL.
+        The asset specified in the URL.
     """
-    with open(f"assets/css/{css_file_name}") as css_file:
-        return Response(css_file.read(), mimetype="text/css")
+    with open(f"assets/{asset_file_name}") as asset_file:
+        mime_type = mimetypes.guess_type(asset_file_name)
+        return Response(asset_file.read(), mimetype=mime_type)
 
 
 if os.getenv("GITHUB_ACTIONS") is not None:
--- template.py
+++ template.py
@@ -2,9 +2,6 @@
 
 import yaml
 
-with open("assets/template.html") as template_code:
-    base_html = template_code.read()
-
 
 def compile_template(config_file):
     """Compile a template into an HTML file.
@@ -16,25 +13,26 @@ def compile_template(config_file):
         A string containing the compiled HTML.
     """
     config_file = yaml.safe_load(config_file)
-    return (
-        base_html.replace("=title=", config_file["title"])
-        .replace("=content=", config_file["content"])
-        .replace(
-            "=styles=",
-            "\n".join(
-                [
-                    f"<link rel='stylesheet' href='/css/{style}'/>"
-                    for style in config_file["styles"]
-                ]
-            ),
-        )
-        .replace(
-            "=scripts=",
-            "\n".join(
-                [
-                    f"<script src='{script}'></script>"
-                    for script in config_file["scripts"]
-                ]
-            ),
-        )
+    styles = "\n".join(
+        [
+            f"<link rel='stylesheet' href='/asset/css/{style}'/>"
+            for style in config_file["styles"]
+        ]
     )
+    scripts = "\n".join(
+        [f"<script src='/asset/js/{script}'></script>" for script in config_file["scripts"]]
+    )
+    return f"""
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>{config_file["title"]}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        {styles}
+    </head>
+    <body>
+        {config_file["content"]}
+        {scripts}
+    </body>
+</html>
+"""
