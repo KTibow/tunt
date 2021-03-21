7b64c2486495faea55178cc4f52c9217386db9f3
Kendell R <KTibow@users.noreply.github.com>
2021-03-21 07:55:16 -0700

    Finish up implementing music

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,19 @@
-window.onload = () => {
-  document.body.onclick = () => {
+window.audioHasPlayed = false;
+window.shouldPlayAudio = !JSON.parse(localStorage.getItem("audioDisabled"));
+document.body.onclick = () => {
+  if (!window.audioHasPlayed && window.shouldPlayAudio)
     document.querySelector("audio").play();
-  };
-}
-window.onload();
+  window.audioHasPlayed = true;
+};
+
 document.querySelector(".audioControls span").onclick = () => {
   document.querySelector("body").style.justifyContent = "center";
   document.querySelector("main").style.display = "none";
   document.querySelector(".audioControlMenu").style.display = "unset";
-}
+};
+
+document.querySelector(".switch input").checked = !window.shouldPlayAudio;
+document.querySelector(".switch input").onclick = () => {
+  window.shouldPlayAudio = !window.shouldPlayAudio;
+  localStorage.setItem("audioDisabled", !window.shouldPlayAudio);
+};
