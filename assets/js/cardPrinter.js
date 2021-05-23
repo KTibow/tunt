1e89d96f7d4d806923eb2911a4e166fff304c9b5
Kendell R <KTibow@users.noreply.github.com>
2021-05-23 14:33:19 -0700

    some stuff

--- assets/js/cardPrinter.js
+++ assets/js/cardPrinter.js
@@ -1,5 +1,11 @@
+function preloadImage(source) {
+  var imageObj = new Image();
+  imageObj.src = source;
+}
 function getCardsReady(howManyCards) {
   var randomIds = [];
+  var currentRow = document.createElement("div");
+  currentRow.classList.add("row");
   for (var i = 0; i < howManyCards; i++) {
     var cardClone = document.querySelector("template").content.cloneNode(true);
     var cardId;
@@ -7,10 +13,30 @@ function getCardsReady(howManyCards) {
       cardId = Math.floor(Math.random() * 5 * howManyCards);
     } while (randomIds.includes(cardId));
     randomIds.push(cardId);
-    cardClone.querySelector(
-      "img"
-    ).src = `https://bwipjs-api.metafloor.com/?bcid=qrcode&text=${cardId}`;
-    document.querySelector(".printShow").appendChild(cardClone);
+    // Fill in QR code
+    var imageSource = `https://bwipjs-api.metafloor.com/?bcid=qrcode&text=${cardId}`;
+    cardClone.querySelector("img").src = imageSource;
+    preloadImage(imageSource);
+    preloadImage.src = cardClone.querySelector("img").src;
+    // Add element
+    currentRow.appendChild(cardClone);
+    if (currentRow.childElementCount == 4) {
+      document.querySelector(".printShow").appendChild(currentRow);
+      currentRow = document.createElement("div");
+      currentRow.classList.add("row");
+    }
+  }
+  if (currentRow.childElementCount > 0) {
+    document.querySelector(".printShow").appendChild(currentRow);
   }
   return randomIds;
+}
+window.onbeforeprint = () => {
+  alert("Make sure to check Background graphics in the settings.");
+};
+function createCards() {
+  var cardNumber = document.querySelector("input").value;
+  getCardsReady(cardNumber);
+  // Wait for images to load
+  setTimeout(window.print, cardNumber * 50);
 }
